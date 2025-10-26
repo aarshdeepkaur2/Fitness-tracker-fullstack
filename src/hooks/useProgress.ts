069d385463d54099f5ebc84a17fcf69a90c543ea
Kiranjeet Kaur Sidhu <kiransidu811@gmail.com>
2025-10-26 16:07:47 -0500

    feat: create useProgress custom hook to manage goal state and actions

---
+++ src/hooks/useProgress.ts
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import type  { ProgressItem } from "../data/progressData";
+import { progressService } from "../services/progressService";
+
+export function useProgress() {
+  const [progress, setProgress] = useState<ProgressItem[]>(progressService.getAllProgress());
+
+
+  const refreshList = () => setProgress([...progressService.getAllProgress()]);
+
+
+  const addGoal = (goal: string) => {
+    progressService.addNewGoal(goal);
+    refreshList();
+  };
+
+  const markNotStarted = (id: number) => {
+    progressService.changeStatus(id, "Not Started");
+    refreshList();
+  };
+
+  const markInProgress = (id: number) => {
+    progressService.changeStatus(id, "In Progress");
+    refreshList();
+  };
+
+  const markCompleted = (id: number) => {
+    progressService.changeStatus(id, "Completed");
+    refreshList();
+  };
+
+  const removeGoal = (id: number) => {
+    progressService.removeGoal(id);
+    refreshList();
+  };
+
+  return { progress, addGoal, markNotStarted, markInProgress, markCompleted, removeGoal };
+}
